bfcda1d692eed53d5b6ed61780fc23f76749414e
UrMjsty <alexciornyi@gmail.com>
2025-03-12 10:19:36 +0200

    Add help menu

---
+++ go2web.py
@@ -0,0 +1,15 @@
+import sys
+
+def show_help():
+    print("Usage:")
+    print("  go2web -u <URL>         # Fetch a URL")
+    print("  go2web -s <search-term> # Search for a term")
+    print("  go2web -h               # Show help")
+
+
+def main():
+    if len(sys.argv) < 2:
+        show_help()
+        return
+if __name__ == "__main__":
+        main()
